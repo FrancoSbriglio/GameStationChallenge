c8c7a27ff5c072ad44e7eb37b01d64ab1cc1e6df
Franco Sbriglio <franco.sbriglio@gmail.com>
2022-03-03 15:57:25 -0300

    Se agregan validaciones en el model input

--- GamesStationChallenge/Models/Input/ComprarItemModelInput.cs
+++ GamesStationChallenge/Models/Input/ComprarItemModelInput.cs
@@ -1,21 +1,33 @@
-﻿using Spv.GamesStation.Repositorio.Entidades.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using Spv.GamesStation.Repositorio.Entidades.Models;
 
 namespace GamesStationChallenge.Models.Input{
 
 public class ComprarItemModelInput
 {
+    [Required(ErrorMessage = "El id Item es requerido")]
     public int IdItem { get; set; }
-    public int IdUsuario { get; set; }
-    public decimal MonedasUsuario { get; set; }
-    public int DiamantesUsuario { get; set; }
-    public int ValorItemDiamantes { get; set; }
-    public int ValorItemMonedas { get; set; }
-    public int IdGuerrero { get; set; }
-    public Fuerza FuerzaGuerrero { get; set; }
-    public Resistencia ResistenciaGuerrero { get; set; }
-    public Velocidad VelocidadGuerrero { get; set; }
-
-    public int IdTipoItem { get; set; }
+    [Required(ErrorMessage = "El id del usuario es requerido")]
+        public int IdUsuario { get; set; }
+        [Required(ErrorMessage = "Las monedas que tiene el usuario son requeridas")]
+        public decimal MonedasUsuario { get; set; }
+        [Required(ErrorMessage = "Los diamantes que tiene el usuario son requeridas")]
+        public int DiamantesUsuario { get; set; }
+        [Required(ErrorMessage = "El valor del item en diamantes son requeridas")]
+        [Range(1, 999999999999, ErrorMessage = "El rango varia entre 0 y 2147483647 ")]
+        public int ValorItemDiamantes { get; set; }
+        [Required(ErrorMessage = "El valor del item en monedas son requeridas")]
+        public int ValorItemMonedas { get; set; }
+        [Required(ErrorMessage = "El id del guerrero es requerido")]
+        public int IdGuerrero { get; set; }
+        [Required(ErrorMessage = "La fuerza del guerrero es requerida")]
+        public Fuerza FuerzaGuerrero { get; set; }
+        [Required(ErrorMessage = "La resistencia del guerrero es requerida")]
+        public Resistencia ResistenciaGuerrero { get; set; }
+        [Required(ErrorMessage = "La velocidad del guerrero es requerida")]
+        public Velocidad VelocidadGuerrero { get; set; }
+        [Required(ErrorMessage = "El tipo del item es requerido")]
+        public int IdTipoItem { get; set; }
  
 
 }
