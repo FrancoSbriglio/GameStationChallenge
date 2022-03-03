4bfe136c3953b61ebbc24aab83e8d45611f9b574
Franco Sbriglio <franco.sbriglio@gmail.com>
2022-03-03 13:10:35 -0300

    Se agrega el dominio con sus respectivos test

--- GamesStationChallenge/Middleware/RequestResponseLoggingMiddleware.cs
+++ GamesStationChallenge/Middleware/RequestResponseLoggingMiddleware.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+﻿using System.Text;
 using Serilog;
 using ServiceStack.Text;
 
--- Spv.GamesStation.Repositorio.Entidades/Models/Resistencia.cs
+++ Spv.GamesStation.Repositorio.Entidades/Models/Resistencia.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Spv.GamesStation.Repositorio.Entidades.Models
+﻿namespace Spv.GamesStation.Repositorio.Entidades.Models
 {
     public  class Resistencia : AtributosComunesGuerrero
     {
